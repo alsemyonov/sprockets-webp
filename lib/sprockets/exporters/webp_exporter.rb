f88cc9826c929027e3a256a409a4e8b6d59a38dd
Alex Semyonov <alex@semyonov.us>
2019-02-01 15:57:49 +0300

    Do not fail on converting to WebP

--- lib/sprockets/exporters/webp_exporter.rb
+++ lib/sprockets/exporters/webp_exporter.rb
@@ -6,21 +6,23 @@ module Sprockets
     # Generates a `.webp` file using the webp-ffi
     class WebpExporter < Exporters::Base
       def setup
-        @webp_target = "#{ target }.webp"
+        @webp_target = "#{target}.webp"
       end
 
       def skip?(logger)
         if ::File.exist?(@webp_target)
-          logger.debug "Skipping #{ @webp_target }, already exists"
+          logger.debug "Skipping #{@webp_target}, already exists"
           true
         else
-          logger.info "Writing #{ @webp_target }"
+          logger.info "Writing #{@webp_target}"
           false
         end
       end
 
       def call
         ::WebP.encode(target, @webp_target, Sprockets::WebP.encode_options)
+      rescue WebP::EncoderError
+        logger.warn "Skipping #{@webp_target}, converter error"
       end
     end
   end
